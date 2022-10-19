b38f5e7f1d7924f9dd35dc4a4213deeed22b1d5a
Priscillia Archibong <archibongpriscillia@gmail.com>
2022-10-19 21:33:45 +0100

    added models and migrations to my musicapp

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model) :
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+class Song(models.Model) :
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+
+class Lyric(models.Model) :
+    song = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
